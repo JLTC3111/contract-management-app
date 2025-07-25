00bebc754d72e5e68664d2c2600117e0f14b637f
JLTC3111 <dr.herbal2011@gmail.com>
2025-07-25 11:27:12 +0700

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -891,6 +891,7 @@ const Login = () => {
                 onChange={(e) => setPassword(e.target.value)} 
                 required 
                 style={{
+                  WebkitBoxShadow: '0 0 0px 1000px ' + (darkMode ? '#fbfffe' : '#e7fffc') + ' inset', 
                   backgroundColor: darkMode ? '#fbfffe' : '#e7fffc',
                   width: '100%',
                   maxWidth: '92.5%',
